0d4c2bcfbe4a20aa329ab26f95a25945222b4330
husniabad <husni.abad.97@gmail.com>
2025-11-23 17:17:24 +0200

    disable unfold admin template

--- mishwari_server/mishwari_server/settings.py
+++ mishwari_server/mishwari_server/settings.py
@@ -51,7 +51,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'unfold',
+    # 'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
