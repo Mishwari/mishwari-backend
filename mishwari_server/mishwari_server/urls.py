b83a46086124a60328f0f5e57990f2ccb85c8335
husniabad <husni.abad.97@gmail.com>
2025-12-14 16:00:17 +0200

    fix admin staticfiles

--- mishwari_server/mishwari_server/urls.py
+++ mishwari_server/mishwari_server/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,4 +13,4 @@ urlpatterns = [
     path('api/', include("wallet.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
